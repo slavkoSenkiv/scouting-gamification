03f820522aac7b8e2ab34e2d02cca4ddaf34b6bb
slavkoSenkiv <60449898+slavkoSenkiv@users.noreply.github.com>
2021-11-28 21:14:15 +0200

    upd

--- migrate and upd data.py
+++ migrate and upd data.py
@@ -19,23 +19,12 @@ db_sheet = gs.worksheet('db')
 # <editor-fold desc="functions">
 def get_col_num(col_name_str, sheet):
     sheet_titles_list = sheet.row_values(1)
-    sheet_lower_titles_list = [title.lower() for title in sheet_titles_list]
 
-    if col_name_str in sheet_lower_titles_list:
-        col_num = sheet_lower_titles_list.index(col_name_str)
+    if col_name_str in sheet_titles_list:
+        col_num = sheet_titles_list.index(col_name_str) + 1
     else:
         col_num = 'x'
-    return col_num + 1
-
-
-def activity_value(activity):
-    if activity == 'гурткові сходини':
-        value = 10
-    elif activity == 'курінні сходини':
-        value = 15
-    else:
-        value = 5
-    return value
+    return col_num
 
 
 def define_score_origin(sheet_name):
@@ -43,15 +32,20 @@ def define_score_origin(sheet_name):
         return 'team'
 
 
-def optimised_migrate_scores_from_team_sheets_and_add_additional_data_from_db_sheet_to_log_sheet():
+def migrate_scores_from_assessment_sheets():
+    list_of_dicts_from_db_sheet = db_sheet.get_all_records()
     for assessment_sheet in gs.worksheets():
-        if assessment_sheet.title.startswith('team'):
+        if 'team' in assessment_sheet.title:
+            first_not_parsed_row_num = len(assessment_sheet.col_values(get_col_num('parsed', assessment_sheet))) + 1
+            last_row = len(assessment_sheet.col_values(get_col_num('Електронна адреса', assessment_sheet)))
+            print('first_not_parsed_row_num', first_not_parsed_row_num)
+            print('last_row', last_row)
             list_of_dicts_from_assessment_sheet = assessment_sheet.get_all_records()
             list_of_lists_for_log_sheet_upd = []
             for dict_row_from_assessment_sheet in list_of_dicts_from_assessment_sheet:
                 for scout_mail in dict_row_from_assessment_sheet:
                     if '.com' in scout_mail:
-                        # <editor-fold desc="getting dict_row_from_assessment_sheet time values">
+                        # <editor-fold desc="getting values from dict_row_from_assessment_sheet row">
                         time_string = dict_row_from_assessment_sheet['Позначка часу']
                         time_stamp = datetime.strptime(time_string, '%d.%m.%Y %H:%M:%S')
                         year = time_stamp.year - 2000
@@ -61,12 +55,13 @@ def optimised_migrate_scores_from_team_sheets_and_add_additional_data_from_db_sh
                         month_week = time_stamp.isocalendar()[1] - time_stamp.replace(day=1).isocalendar()[1] + 1
                         year_week = time_stamp.isocalendar().week
                         hour = time_stamp.hour
-                        # </editor-fold>
                         score_origin = define_score_origin(assessment_sheet.title)
-                        # print(year, month, month_day, week_day, month_week, year_week, hour, score_origin)
                         who_evaluate = dict_row_from_assessment_sheet['Електронна адреса']
                         activity = dict_row_from_assessment_sheet['activity']
                         scout_score = dict_row_from_assessment_sheet[scout_mail]
+                        if scout_score == '':
+                            continue
+                        # </editor-fold>
                         new_log_row_note = [time_string,
                                             scout_mail,
                                             scout_score,
@@ -80,98 +75,24 @@ def optimised_migrate_scores_from_team_sheets_and_add_additional_data_from_db_sh
                                             year_week,
                                             hour,
                                             score_origin]
+                        for dict_row in list_of_dicts_from_db_sheet:
+                            if scout_mail in dict_row.values():
+                                new_log_row_note.append(dict_row['first name'])
+                                new_log_row_note.append(dict_row['second name'])
+                                new_log_row_note.append(dict_row['sex'])
+                                new_log_row_note.append(dict_row['team'])
+                                new_log_row_note.append(dict_row['troop'])
+                                new_log_row_note.append(dict_row['joined'])
+                                new_log_row_note.append(dict_row['born'])
+                                new_log_row_note.append(dict_row['age group'])
+                                new_log_row_note.append(dict_row['rank'])
                         list_of_lists_for_log_sheet_upd.append(new_log_row_note)
-                        # print(log_row)
-            # ______
-            list_of_dicts_from_db_sheet = db_sheet.get_all_records()
-            first_not_parsed_row_num = len(log_sheet.col_values(get_col_num('parsed', log_sheet))) + 1
-            range_to_update = f'N{str(first_not_parsed_row_num)}:W{str(len(log_sheet.col_values(2)))}'
-            list_of_lists_for_log_sheet_upd = []
-            for scout_mail in log_sheet.col_values(get_col_num('mail', log_sheet)):
-                for dict_row in list_of_dicts_from_db_sheet:
-                    if scout_mail in dict_row.values():
-                        scout_list = [dict_row['first name'],
-                                      dict_row['second name'],
-                                      dict_row['sex'],
-                                      dict_row['team'],
-                                      dict_row['troop'],
-                                      dict_row['joined'],
-                                      dict_row['born'],
-                                      dict_row['age group'],
-                                      dict_row['rank'],
-                                      True]  # parsed?
-                        list_of_lists_for_log_sheet_upd.append(scout_list)
 
-            log_sheet.update(range_to_update, list_of_lists_for_log_sheet_upd)
             first_empty_row_num = len(log_sheet.col_values(1)) + 1
             future_last_row_num = first_empty_row_num + len(list_of_lists_for_log_sheet_upd)
-            update_range = f'A{str(first_empty_row_num)}:M{str(future_last_row_num)}'
+            update_range = f'A{str(first_empty_row_num)}:V{str(future_last_row_num)}'
+            print(pprint.pformat(list_of_lists_for_log_sheet_upd))
             log_sheet.update(update_range, list_of_lists_for_log_sheet_upd)
-            gs.values_clear(f"{assessment_sheet.title}!A2:J20")
-
-
-def migrate_scores_from_group_sheets_to_log_sheet():
-    for assessment_sheet in gs.worksheets():
-        if assessment_sheet.title.startswith('team'):
-            list_of_dicts_from_assessment_sheet = assessment_sheet.get_all_records()
-            list_of_lists_for_log_sheet_upd = []
-            for dict_row_from_assessment_sheet in list_of_dicts_from_assessment_sheet:
-                for scout_mail in dict_row_from_assessment_sheet:
-                    if '.com' in scout_mail:
-                        # <editor-fold desc="getting dict_row_from_assessment_sheet time values">
-                        time_string = dict_row_from_assessment_sheet['Позначка часу']
-                        time_stamp = datetime.strptime(time_string, '%d.%m.%Y %H:%M:%S')
-                        year = time_stamp.year - 2000
-                        month = time_stamp.month
-                        month_day = time_stamp.day
-                        week_day = calendar.day_name[time_stamp.weekday()]
-                        month_week = time_stamp.isocalendar()[1] - time_stamp.replace(day=1).isocalendar()[1] + 1
-                        year_week = time_stamp.isocalendar().week
-                        hour = time_stamp.hour
-                        # </editor-fold>
-                        score_origin = define_score_origin(assessment_sheet.title)
-                        # print(year, month, month_day, week_day, month_week, year_week, hour, score_origin)
-                        who_evaluate = dict_row_from_assessment_sheet['Електронна адреса']
-                        activity = dict_row_from_assessment_sheet['activity']
-                        scout_score = dict_row_from_assessment_sheet[scout_mail]
-                        new_log_row_note = [time_string,
-                                            scout_mail,
-                                            scout_score,
-                                            activity,
-                                            who_evaluate,
-                                            year,
-                                            month,
-                                            month_day,
-                                            week_day,
-                                            month_week,
-                                            year_week,
-                                            hour,
-                                            score_origin]
-                        list_of_lists_for_log_sheet_upd.append(new_log_row_note)
-                        # print(log_row)
-
-
-def migrate_additional_data_to_log_sheet():
-    list_of_dicts_from_db_sheet = db_sheet.get_all_records()
-    first_not_parsed_row_num = len(log_sheet.col_values(get_col_num('parsed', log_sheet))) + 1
-    range_to_update = f'N{str(first_not_parsed_row_num)}:W{str(len(log_sheet.col_values(2)))}'
-    list_of_lists_for_log_sheet_upd = []
-    for scout_mail in log_sheet.col_values(get_col_num('mail', log_sheet)):
-        for dict_row in list_of_dicts_from_db_sheet:
-            if scout_mail in dict_row.values():
-                scout_list = [dict_row['first name'],
-                              dict_row['second name'],
-                              dict_row['sex'],
-                              dict_row['team'],
-                              dict_row['troop'],
-                              dict_row['joined'],
-                              dict_row['born'],
-                              dict_row['age group'],
-                              dict_row['rank'],
-                              True]  # parsed?
-                list_of_lists_for_log_sheet_upd.append(scout_list)
-
-    log_sheet.update(range_to_update, list_of_lists_for_log_sheet_upd)
 
 
 def gather_sum_update_scores_from_main_sheet():
@@ -209,11 +130,20 @@ def gather_sum_update_scores_from_main_sheet():
 
 
 # </editor-fold>
-optimised_migrate_scores_from_team_sheets_and_add_additional_data_from_db_sheet_to_log_sheet()
+migrate_scores_from_assessment_sheets()
 # migrate_scores_from_group_sheets_to_log_sheet()
 # migrate_additional_data_to_log_sheet()
 # gather_sum_update_scores_from_main_sheet()
-
+"""
+def activity_value(activity):
+    if activity == 'гурткові сходини':
+        value = 10
+    elif activity == 'курінні сходини':
+        value = 15
+    else:
+        value = 5
+    return value
+"""
 
 
 
--- scratch.py
+++ scratch.py
@@ -13,59 +13,9 @@ google_client = gspread.oauth(credentials_filename=path_to_gspread_credentials_j
 gs = google_client.open(gs_name)
 log_sheet = gs.worksheet('log')
 db_sheet = gs.worksheet('db')
+assessment_sheet = gs.worksheet('team')
 # </editor-fold>
 
+test_data = assessment_sheet.batch_get(('A1:A2',))
+print(test_data)
 
-def week_number_of_month(date_value):
-    return (date_value.isocalendar()[1] - date_value.replace(day=1).isocalendar()[1] + 1)
-
-
-def define_score_origin(sheet_name):
-    if sheet_name.startswith('team'):
-        return 'team'
-
-
-def migrate_scores_from_group_sheets_to_log_sheet():
-    for group_evaluation_sheet in gs.worksheets():
-        if group_evaluation_sheet.title.startswith('team'):
-            list_of_dicts = group_evaluation_sheet.get_all_records()
-            log_sheet_list_of_lists_update = []
-            for dict_row in list_of_dicts:
-                for scout_key in dict_row:
-                    if '.com' in scout_key:
-                        # <editor-fold desc="get time values">
-                        time_string = dict_row['Позначка часу']
-                        time_stamp = datetime.strptime(time_string, '%d.%m.%Y %H:%M:%S')
-                        year = time_stamp.year
-                        month = time_stamp.month
-                        month_day = time_stamp.day
-                        week_day = calendar.day_name[time_stamp.weekday()]
-                        month_week = week_number_of_month(time_stamp)
-                        year_week = time_stamp.isocalendar().week
-                        hour = time_stamp.hour
-                        # </editor-fold>
-                        score_origin = define_score_origin(group_evaluation_sheet.title)
-                        print(year, month, month_day, week_day, month_week, year_week, hour, score_origin)
-
-                        who_evaluate = dict_row['Електронна адреса']
-                        activity = dict_row['activity']
-                        scout = scout_key
-                        scout_score = dict_row[scout_key]
-                        log_row = [time_stamp,
-                                   scout,
-                                   scout_score,
-                                   activity,
-                                   who_evaluate,
-                                   year,
-                                   month,
-                                   month_day,
-                                   week_day,
-                                   month_week,
-                                   year_week,
-                                   hour,
-                                   score_origin]
-                        log_sheet_list_of_lists_update.append(log_row)
-                        # print(log_row)
-
-
-migrate_scores_from_group_sheets_to_log_sheet()
